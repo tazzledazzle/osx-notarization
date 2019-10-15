3e2f113f8efefe32ee6da0f96fc33447967bd270
Terence Schumacher <tschumacher@tableau.com>
2019-10-15 13:28:53 -0700

    data class organization

--- buildSrc/src/main/kotlin/NotarizationPlugin.kt
+++ buildSrc/src/main/kotlin/NotarizationPlugin.kt
@@ -555,7 +555,12 @@ open class NotarizationPluginExtension {
     var certificateId: String? = null
 }
 
-data class NotarizationInfo(val bundleId: String = "", val requestUUID: String = "",
-                            val statusMsg: String = "", val status: String = "", val logFileUrl: String = "",
-                            val statusCode: Boolean = false, val notarizationExt: NotarizationPluginExtension = NotarizationPluginExtension()
+data class NotarizationInfo (
+    val bundleId: String = "",
+    val requestUUID: String = "",
+    val statusMsg: String = "",
+    val status: String = "",
+    val logFileUrl: String = "",
+    val statusCode: Boolean = false,
+    val notarizationExt: NotarizationPluginExtension = NotarizationPluginExtension()
 )
