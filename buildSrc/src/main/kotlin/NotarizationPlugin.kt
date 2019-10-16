23fd4c6014d0130c6224114eb159e244e90dc718
builder <builder@dvvmnotarize01.tsi.lan>
2019-10-16 13:24:44 -0700

    debugging locally

--- buildSrc/src/main/kotlin/NotarizationPlugin.kt
+++ buildSrc/src/main/kotlin/NotarizationPlugin.kt
@@ -567,11 +567,11 @@ open class NotarizationPluginExtension {
 }
 
 data class NotarizationInfo (
-    val bundleId: String = "",
-    val requestUUID: String = "",
-    val statusMsg: String = "",
-    val status: String = "",
-    val logFileUrl: String = "",
-    val statusCode: Boolean = false,
-    val notarizationExt: NotarizationPluginExtension = NotarizationPluginExtension()
+    val bundleId: String? = "",
+    val requestUUID: String? = "",
+    val statusMsg: String? = "",
+    val status: String? = "",
+    val logFileUrl: String? = "",
+    val statusCode: Boolean? = false,
+    val notarizationExt: NotarizationPluginExtension? = NotarizationPluginExtension()
 )
